256536660d32ef662c2fcc84480ada56875eedb3
moixocreative <moixo.creative@gmail.com>
2025-08-04 21:14:27 +0100

    fix: remove @typescript-eslint/recommended to resolve eslint config error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2022,
